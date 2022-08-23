8d097e784f9f620dbec38cd4394bb48446f3916f
JhonTexter29 <111344210+JhonTexter29@users.noreply.github.com>
2022-08-23 14:40:51 +0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,49 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+        knight.sayMyStrength();
+        knight.sayMyAgility();
+        knight.sayMyIntelligence();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+        mage.sayMyStrength();
+        mage.sayMyAgility();
+        mage.sayMyIntelligence();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+        thief.sayMyStrength();
+        thief.sayMyAgility();
+        thief.sayMyIntelligence();
+
+        Character carry = new Character();
+
+        carry.strength = 10;     
+        carry.agility = 6;
+        carry.intelligence = 5;
+        carry.name = "juggernaut";
+        carry.sayMyName();
+        carry.sayMyStrength();
+        carry.sayMyAgility();
+        carry.sayMyIntelligence();
+
+    }
+}
